0038c6c17e2e04d62d69a36f1202d2006ec3c5c3
Kareem <117325791+kareem1099@users.noreply.github.com>
2025-06-12 23:16:47 +0300

    >>>

--- tamenny_app-main/lib/core/services/post_tag_prediction_service.dart
+++ tamenny_app-main/lib/core/services/post_tag_prediction_service.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 
 class PredictionService {
   final Dio _dio = Dio();
-  final String _baseUrl ="https://4f68-197-38-123-89.ngrok-free.app/predict_Text" ;
+  final String _baseUrl ="https://a937-197-38-135-169.ngrok-free.app/predict_Text" ;
 
   Future<String?> predict(String text) async {
     try {
