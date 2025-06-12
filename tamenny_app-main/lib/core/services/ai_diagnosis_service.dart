f67f5a3d7e28faf6fb017b68e300cfb7cea6d087
Kareem <117325791+kareem1099@users.noreply.github.com>
2025-06-12 23:04:13 +0300

    >>>

--- tamenny_app-main/lib/core/services/ai_diagnosis_service.dart
+++ tamenny_app-main/lib/core/services/ai_diagnosis_service.dart
@@ -3,7 +3,7 @@ import 'package:dio/dio.dart';
 
 class AIDiagnosisService {
   final Dio dio;
-  final String baseUrl = 'https://4f68-197-38-123-89.ngrok-free.app';
+  final String baseUrl = 'https://a937-197-38-135-169.ngrok-free.app';
   AIDiagnosisService(this.dio);
 
   Future<Map<String, dynamic>> Diagnosis({
